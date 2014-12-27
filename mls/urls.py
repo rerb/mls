062ee36b20557b88daf45ee05cef1d3bae6a7d9c
rerb <rerb@mbp.local>
2014-12-27 11:58:28 -0800

    Add Tag API urls and listen on api/ rather than /.

--- mls/urls.py
+++ mls/urls.py
@@ -9,10 +9,11 @@ from server import views
 router = routers.DefaultRouter()
 router.register(r'comments', views.CommentViewSet)
 router.register(r'languages', views.LanguageViewSet)
+router.register(r'tags', views.TagViewSet)
 router.register(r'votes', views.VoteViewSet)
 
 urlpatterns = patterns('',
-                       url(r'^', include(router.urls)),
+                       url(r'^api/', include(router.urls)),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^api-auth/',
                            include('rest_framework.urls',
